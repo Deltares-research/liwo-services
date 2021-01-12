e6754cdc5d4c843423c40f558fbdba14e12a3f69
Fedor Baart <fedor.baart@deltares.nl>
2021-01-12 14:53:29 +0100

    fix tests

---
+++ liwo_services/cli.py
@@ -0,0 +1,26 @@
+"""Console script for liwo_services."""
+import sys
+import logging
+
+import click
+
+import liwo_services.main
+
+from flask.cli import FlaskGroup
+
+logging.basicConfig(
+    level=logging.INFO
+)
+
+
+def create_app():
+    # return the created wsgi app
+    return liwo_services.main.app
+
+@click.group(cls=FlaskGroup, create_app=create_app)
+def cli():
+    """Management script for the liwo_services application application."""
+    pass
+
+if __name__ == "__main__":
+    sys.exit(cli())  # pragma: no cover
