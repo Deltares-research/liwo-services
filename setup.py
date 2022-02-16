be9e24441f8c8a60e8f19d4c624f1a768a3cbe4a
Maarten Pronk <git@evetion.nl>
2022-02-16 18:19:55 +0100

    feat: Add automatic bump, changelog and release with commitizen.

--- setup.py
+++ setup.py
@@ -2,52 +2,60 @@
 
 """The setup script."""
 
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 
-with open('README.rst') as readme_file:
+with open("README.rst") as readme_file:
     readme = readme_file.read()
 
-with open('HISTORY.rst') as history_file:
+with open("HISTORY.rst") as history_file:
     history = history_file.read()
 
-requirements = ['Click>=7.0', ]
+requirements = [
+    "Click>=7.0",
+]
 
-setup_requirements = ['pytest-runner', ]
+setup_requirements = [
+    "pytest-runner",
+]
 
-test_requirements = ['pytest>=3', ]
+test_requirements = [
+    "pytest>=3",
+]
+
+version = "v0.1.1"
 
 setup(
     author="Fedor Baart",
-    author_email='fedor.baart@deltares.nl',
-    python_requires='>=3.5',
+    author_email="fedor.baart@deltares.nl",
+    python_requires=">=3.5",
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
     description="LIWO Backend Services",
     entry_points={
-        'console_scripts': [
-            'liwo_services=liwo_services.cli:cli',
+        "console_scripts": [
+            "liwo_services=liwo_services.cli:cli",
         ],
     },
     install_requires=requirements,
     license="GNU General Public License v3",
-    long_description=readme + '\n\n' + history,
+    long_description=readme + "\n\n" + history,
     include_package_data=True,
-    keywords='liwo_services',
-    name='liwo_services',
-    packages=find_packages(include=['liwo_services', 'liwo_services.*']),
+    keywords="liwo_services",
+    name="liwo_services",
+    packages=find_packages(include=["liwo_services", "liwo_services.*"]),
     setup_requires=setup_requirements,
-    test_suite='tests',
+    test_suite="tests",
     tests_require=test_requirements,
-    url='https://github.com/Deltares/liwo_services',
-    version='0.1.0',
+    url="https://github.com/Deltares/liwo_services",
+    version=version,
     zip_safe=False,
 )
